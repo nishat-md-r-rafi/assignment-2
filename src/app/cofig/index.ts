8529ab17d38d55a0019433d1c9aee4bed5c5b359
unknown <r.rafi1615005@gmail.com>
2023-11-25 10:26:41 +0600

    run your app

--- src/app/cofig/index.ts
+++ src/app/cofig/index.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: path.join(process.cwd() + '.env') });
+dotenv.config({ path: path.join((process.cwd(), '.env')) });
+console.log(process.cwd());
 
 export default {
   port: process.env.PORT,
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@ import mongoose from 'mongoose';
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(
+      'mongodb+srv://rafi:rafi005@cluster0.mqo07.mongodb.net/express-assignment-2?retryWrites=true&w=majority',
+    );
     app.listen(config.port, () => {
       console.log('App is listening port', config.port);
     });
